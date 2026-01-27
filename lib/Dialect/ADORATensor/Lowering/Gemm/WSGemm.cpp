f49743e0196bc0601df4773b32b8cc6f01c46e6f
mion <1063500258@qq.com>
2026-01-27 12:12:17 +0800

    add memref.alloc and memref.copy to wsgemm lower. not test.

--- lib/Dialect/ADORATensor/Lowering/Gemm/WSGemm.cpp
+++ lib/Dialect/ADORATensor/Lowering/Gemm/WSGemm.cpp
@@ -540,6 +540,22 @@ AffineForOp TiledWeightStationaryGemm(
   K_step = (tilecol * K_temporal_tile);
   M_step = M_temporal_tile;
 
+  //==========================================================
+  // Allocate output buffer (type comes from GEMM output, NOT C)
+  //==========================================================
+  Location loc = op.getLoc();
+
+  auto outTy = op.getO().getType().dyn_cast<MemRefType>();
+  assert(outTy && "Expected GemmOp to return memref as output in lowering.");
+
+  opbuilder.setInsertionPointAfter(op.getOperation());
+  Value out = opbuilder.create<memref::AllocOp>(loc, outTy);
+
+  //==========================================================
+  // Initialize out with C (copy if same shape, else broadcast init)
+  //==========================================================
+  mlir::Operation* InitializationOp = initOutWithC2DLike(opbuilder, loc, out, op.getC(), ArrayRef<int64_t>({ShapeA[0], ShapeA[1]}));
+
   //////////////////////////////////////
   /// Generate systolic gemm
   //////////////////////////////////////
@@ -554,8 +570,9 @@ AffineForOp TiledWeightStationaryGemm(
       )
     );
   
-  op.getOperation()->getBlock()->push_back(loop);
-  loop.getOperation()->moveAfter(op);
+  // op.getOperation()->getBlock()->push_back(loop);
+  // loop.getOperation()->moveAfter(op);
+  loop.getOperation()->moveAfter(InitializationOp);
   // } 
   // else{
   //   loop = GenerateTiledNestedLoopWithoutLoopCarry(
